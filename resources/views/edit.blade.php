fb5e7e176710edff9ebb8029ee40e8810c915e86
FrostVin108 <vincentsidarta.hu@gmail.com>
2024-07-31 14:34:49 +0700

    code+

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -13,23 +13,44 @@
         @method('PUT')
           <div class="form-group">
             <label for="exampleInputEmail1">Description</label>
-            <input type="text" class="form-control" placeholder="Enter Description" name="description" value="{{ old('description', $obatmodel->description) }}">
+            <input type="text" class="form-control @error('stock') is-invalid @enderror" placeholder="Enter Description" name="description" value="{{ old('description', $obatmodel->description) }}">
           </div>
+          @error('description')
+          <div class="alert alert-danger mt-2">
+              {{ $message }}
+          </div>
+          @enderror
 
           <div class="form-group">
             <label for="exampleInputEmail1">UOM</label>
-            <input type="text" class="form-control" placeholder="Enter UOM" name="uom"  value="{{ old('uom', $obatmodel->uom) }}">
+            <input type="text" class="form-control @error('stock') is-invalid @enderror" placeholder="Enter UOM" name="uom"  value="{{ old('uom', $obatmodel->uom) }}">
           </div>
+          @error('uom')
+          <div class="alert alert-danger mt-2">
+              {{ $message }}
+          </div>
+          @enderror
 
           <div class="form-group">
             <label for="exampleInputEmail1">Item Code</label>
-            <input type="number" class="form-control" placeholder="Enter Item Code" name="item_code" value="{{ old('item_code', $obatmodel->item_code) }}">
+            <input type="number" class="form-control @error('stock') is-invalid @enderror" placeholder="Enter Item Code" name="item_code" value="{{ old('item_code', $obatmodel->item_code) }}">
           </div>
+          @error('item_code')
+          <div class="alert alert-danger mt-2">
+              {{ $message }}
+          </div>
+          @enderror
 
           <div class="form-group">
             <label for="exampleInputEmail1">Stock</label>
-            <input type="number" class="form-control" placeholder="Enter Stock On Hand" name="stock" value="{{ old('stock', $obatmodel->stock) }}">
+            <input type="number" class="form-control @error('stock') is-invalid @enderror" placeholder="Enter Stock On Hand" name="stock" value="{{ old('stock', $obatmodel->stock) }}">
           </div>
+            @error('stock')
+              <div class="alert alert-danger mt-2">
+                  {{ $message }}
+              </div>
+            @enderror
+
               <button type="submit" class="btn btn-success">Update</button>
               <button type="reset" class="btn btn-success">Reset</button>
               <a href="{{ route('obat.home') }}" class="btn btn-warning">Return</a>
