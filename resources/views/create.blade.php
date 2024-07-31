0ad34e392a52fbee7a0f8e88015cf80ba0bbe70c
FrostVin108 <vincentsidarta.hu@gmail.com>
2024-07-31 11:22:42 +0700

    code+

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -14,23 +14,43 @@
                 @method('post')
                   <div class="form-group">
                     <label for="exampleInputEmail1">Description</label>
-                    <input type="text" class="form-control" placeholder="Enter Description" name="description">
+                    <input type="text" class="form-control @error('description') is-invalid @enderror" placeholder="Enter Description" name="description">
                   </div>
+                  @error('description')
+                    <div class="alert alert-danger mt-2">
+                        {{ $message }}
+                    </div>
+                  @enderror
 
                   <div class="form-group">
                     <label for="exampleInputEmail1">UOM</label>
-                    <input type="text" class="form-control" placeholder="Enter UOM" name="uom">
+                    <input type="text" class="form-control @error('uom') is-invalid @enderror" placeholder="Enter UOM" name="uom">
                   </div>
+                  @error('uom')
+                  <div class="alert alert-danger mt-2">
+                      {{ $message }}
+                  </div>
+                  @enderror
 
                   <div class="form-group">
                     <label for="exampleInputEmail1">Item Code</label>
-                    <input type="number" class="form-control" placeholder="Enter Item Code" name="item_code">
+                    <input type="number" class="form-control @error('item_code') is-invalid @enderror" placeholder="Enter Item Code" name="item_code">
                   </div>
+                                @error('item_code')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
 
-                  <div class="form-group">
+                  <div class="form-group" >
                     <label for="exampleInputEmail1">Stock</label>
-                    <input type="number" class="form-control" placeholder="Enter Stock On Hand" name="stock">
+                    <input type="number" class="form-control @error('stock') is-invalid @enderror" placeholder="Enter Stock On Hand" name="stock">
                   </div>
+                  @error('stock')
+                  <div class="alert alert-danger mt-2">
+                      {{ $message }}
+                  </div>
+                  @enderror
 
                       <button type="submit" class="btn btn-success">Submit</button>
                       
